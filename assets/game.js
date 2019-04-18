324978e092e38d0dd2af2d3cca12cbe7dddc4665
jordan <jcs5317@gmail.com>
2019-04-18 16:26:02 -0700

    grade

--- assets/game.js
+++ assets/game.js
@@ -1,52 +1,56 @@
 //variables array of letters to type
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabet = [];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var letterUsed = [];
+var computerGuess = [];
+
+generateAlphabet();
+
+//computer makes a guess
+computerGuess = setGuess();
 
 //key up fucntion to track each letter
 document.onkeyup = function (event) {
-    //guessesLeft--;
+    var userguess = event.key.toLocaleLowerCase();
 
-    var userguess = event.key;
-
-    //console.log(userguess);
-
-    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-    //console.log(computerGuess);
-
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-    if (options.indexOf(userguess) > -1) {
+    if (guessesLeft > 0) {
         if (userguess === computerGuess) {
-            wins++
+            wins++;
             guessesLeft = 9;
-            letterUsed = [];
-        }
-        if (userguess != computerGuess) {
-            guessesLeft--;
-            letterUsed.push(guessesLeft);
-        }
-
-        if (guessesLeft === 0) {
-            guessesLeft = 9;
-            losses++;
-            letterUsed = [];
+            letterUsed.push(userguess);
         }
         else {
-            losses++
-            //console.log("losses" + losses);
+            guessesLeft--;
+            letterUsed.push(userguess);
         }
-
-        var html =
-            "<p>Guess what letter I am thinking of</p>" +
-            "<p>wins: " + wins + "</p>" +
-            "<p>losses: " + losses + "</p>" +
-            "<p>Guesses left: " + guessesLeft + "</p>" +
-            "<p>Your Guesses so far: " + letterUsed.join(", ") + "</p>";
-
-        document.querySelector("#game").innerHTML = html;
     }
-};
+    else {
+        guessesLeft = 9;
+        losses++;
+        letterUsed = [];
+        setGuess = [];
+        //restart would not happen unless refresh browser
+    }
+    //put results to display on html 
+    var html =
+        "<p>Guess what letter I am thinking of</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses left: " + guessesLeft + "</p>" +
+        "<p>Your Guesses so far: " + letterUsed.join(", ") + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
+}
+
+function generateAlphabet() {
+    for (var i = 97; i <= 122; i++) {
+        var userguess = String.fromCharCode(i)
+        alphabet.push(userguess);
+    }
+}
+//randomize computer options
+function setGuess() {
+    return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
